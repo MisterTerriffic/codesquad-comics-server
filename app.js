1c1fc5c45153941dc4c8420588f508814ea154b3
Walter Fisher <o.b.i.e@Mac.lan>
2025-05-11 12:44:04 -0500

    Created the send method for all the routes.

--- app.js
+++ app.js
@@ -17,45 +17,27 @@ app.use(express.static(path.join(__dirname)));
 app.use(express.urlencoded());
 
 app.get("/", (request, response, next) => {
-  response.status(200).json({
-    success: { message: "This route points to the Home page" },
-    statusCode: 200,
-  });
+  response.send("This route points to the Home page");
 });
 
 app.get("/api/books", (request, response, next) => {
-  response.status(200).json({
-    success: { message: "This will send all of the book data" },
-    statusCode: 200,
-  });
+  response.send("This will send all of the book data");
 });
 
 app.get("/api/books/:id", (request, response, next) => {
-  response.status(200).json({
-    success: { message: "This will send a single book by its id" },
-    statusCode: 200,
-  });
+  response.send("This will send a single book by its id");
 });
 
 app.get("/api/books/create/new", (request, response, next) => {
-  response.status(201).json({
-    success: { message: "This will create a new book" },
-    statusCode: 201,
-  });
+  response.send("This will create a new book");
 });
 
 app.get("/api/books/update/:id", (request, response, next) => {
-  response.status(200).json({
-    success: { message: "This will update a book by its id" },
-    statusCode: 200,
-  });
+  response.send("This will update a book by its id");
 });
 
 app.get("/api/books/delete/:id", (request, response, next) => {
-  response.status(200).json({
-    success: { message: "This will delete a book by its id" },
-    statusCode: 200,
-  });
+  response.send("This will delete a book by its id");
 });
 
 app.listen(PORT, () => {
